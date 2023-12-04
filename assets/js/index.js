e1ec800fcf7eb1e35b253e91ffd0ed3fac2f5ff0
Carlos Marin <carlosmarinphs@gmail.com>
2023-12-04 15:46:46 -0500

    I have updated my JS and debugging it at the moment

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,55 +1,72 @@
-
-
-
-
 const selectOptionDropdown = document.getElementById("searchOption");
 const categoryDropdown = document.getElementById("category");
 const categoryDropdownContainer = document.getElementById("categoryDropdown");
 const productListContainer = document.getElementById("productList");
+const searchButton = document.getElementById("searchButton");
+const searchForm = document.getElementById("searchForm");
 
 fetch("http://localhost:8081/api/categories")
   .then((res) => res.json())
   .then((categories) => {
     categories.forEach((category) => {
       const option = document.createElement("option");
-      option.value = category.name;
+      option.value = category.categoryId;
       option.textContent = category.name;
       categoryDropdown.appendChild(option);
     });
   });
 
 selectOptionDropdown.onchange = () => {
-  if (selectOptionDropdown.value === "category") {
+  productListContainer.innerHTML = "";
+
+  if (selectOptionDropdown.value === "byCategory") {
     categoryDropdownContainer.style.display = "block";
   } else {
     categoryDropdownContainer.style.display = "none";
   }
-  if (selectOptionDropdown.value === "all") {
+};
+
+searchButton.onclick = () => {
+  if (selectOptionDropdown.value === "viewAll") {
     fetch("http://localhost:8081/api/products")
       .then((res) => res.json())
       .then((products) => displayProducts(products));
+
+
+  // } else if (selectOptionDropdown.value === "byCategory") {
+  //   const selectedCategoryId = categoryDropdown.value;
+
+    fetch(`http://localhost:8081/api/products?category=${selectedCategoryId}`)
+      .then((res) => res.json())
+      .then((products) => displayProducts(products, selectedCategoryId));
   }
 };
 
 categoryDropdown.onchange = () => {
-  const selectedCategory = categoryDropdown.value;
+  productListContainer.innerHTML = "";
 
-  fetch(`http://localhost:8081/api/products?category=${selectedCategory}`)
+  const selectedCategoryId = categoryDropdown.value;
+
+  fetch(`http://localhost:8081/api/products?category=${selectedCategoryId}`)
     .then((res) => res.json())
-    .then((products) => displayProducts(products));
+    .then((products) => displayProducts(products, selectedCategoryId));
 };
 
-function displayProducts(products) {
+function displayProducts(products, selectedCategoryId) {
   // Clear existing content in productListContainer
   productListContainer.innerHTML = "";
 
+  const categoryIdToMatch = parseInt(selectedCategoryId);
+
   // Display each product
   products.forEach((product) => {
-    const productDiv = document.createElement("div");
-    productDiv.innerHTML = `
+    if (product.categoryId === categoryIdToMatch) {
+      const productDiv = document.createElement("div");
+      productDiv.innerHTML = `
       <p>${product.productId} - ${product.productName} - $${product.unitPrice}</p>
       <a href="product-details.html?productId=${product.productId}">See details</a>
     `;
-    productListContainer.appendChild(productDiv);
+      productListContainer.appendChild(productDiv);
+    }
   });
 }
